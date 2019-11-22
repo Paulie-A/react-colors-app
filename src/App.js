2bce8e6440ac327303fe8bf6b0d5731e73e6a506
Paul August <paul.augustp@gmail.com>
2019-11-22 16:11:49 +0000

    Added unstyled color slider

--- src/App.js
+++ src/App.js
@@ -3,20 +3,12 @@ import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
-// function App() {
-//   return (
-//     <div className="App">
-//      <Palette palette={seedColors[4]} />
-//     </div>
-//   );
-// }
 
 class App extends Component {
   render() {
-    console.log(generatePalette(seedColors[4]) ) ;
     return (
       <div>
-        <Palette {...seedColors[7]} />
+        <Palette palette={generatePalette(seedColors[4])} />
       </div>
     );
   }
