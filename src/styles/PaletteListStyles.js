25edd3745ea38471a03af187303bcc101118fc24
Paul August <paul.augustp@gmail.com>
2019-12-15 17:03:34 +0000

    Add bg svg to PaletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,13 +1,22 @@
 import sizes from './sizes';
+import bg from './bg.svg';
 
 export default {
     root: {
-        backgroundColor: "blue",
+        // backgroundColor: "blue",
         height: "100vh",
         display: "flex",
         alignItems: "flex-start",
         justifyContent: "center",
-        overflow: "scroll"
+        /* background by SVGBackgrounds.com */
+        backgroundColor: "#394bad",
+        backgroundImage: `url(${bg})`,
+        overflow: "scroll",
+        backgroundAttachment: "fixed",
+        backgroundSize: "cover" 
+    },
+    heading: {
+        fontSize: "2rem"
     },
     container: {
         width: "50%",
@@ -43,7 +52,7 @@ export default {
         },
         [sizes.down("xs")]: {
             gridTemplateColumns: "repeat(1, 100%)",
-            gridGap: "1rem",
+            gridGap: "1.4rem",
         }
     }
 };
